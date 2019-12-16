bcd5b85e10add44bcd31614a97fc2c608da79c2e
alokdivya <45335333+alokdivya@users.noreply.github.com>
2019-12-16 09:35:33 +0530

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,24 @@
+//normal function sattement 
+function sayHi(){
+    console.log("Hiii");
+}
+sayHi();
+
+
+
+//Expression function
+
+
+var Bye = function sayBye(){
+        console.log("Byeee");
+    }
+
+Bye();
+
+//Another Expression function
+
+var Bye = function(){
+    console.log("Byeee");
+}
+
+Bye();
